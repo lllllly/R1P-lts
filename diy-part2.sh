0942cc55acfb516404255b3f943e4f6296c2b550
lllllly <2821699450@qq.com>
2022-05-01 19:47:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/KERNEL_PATCHVER=5.15/KERNEL_PATCHVER=5.4/g' target/linux/rockchip/Makefile
+sed -i 's/KERNEL_PATCHVER=5.15/KERNEL_PATCHVER=5.10/g' target/linux/rockchip/Makefile
